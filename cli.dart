efc01d92e612e285cc015fb7c05f692bf8f1a158
Ovidio R. Calvet <calveto@gmail.com>
2019-02-16 21:24:30 -0500

    Move calculator to its own file

--- cli.dart
+++ cli.dart
@@ -1,32 +1,4 @@
-class Calculator {
-  List<int> _values;
-  Calculator() {
-    _values = [];
-  }
-
-  addValue(int val) {
-    _values.add(val);
-  }
-
-  removeValue(int val) {
-    _values.remove(val);
-  }
-
-  sum() {
-    int sum = _values.reduce((v1, v2) => v1 + v2);
-    return sum;
-  }
-
-  @override
-  String toString() {
-    String str = "";
-    for(int n in _values) {
-      str += '${n.toString()} ';
-    }
-    return str;
-  }
-}
-
+import 'calculator.dart';
 
 void main() {
   Calculator calculator = Calculator();
